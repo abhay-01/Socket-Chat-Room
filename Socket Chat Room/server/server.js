5250be7b9bd8c0b33a8e78a97fa2cf920f741cb8
abhay-01 <pratapsinghabhay999@gmail.com>
2023-11-18 16:31:31 +0530

    socket admin

--- Socket Chat Room/server/server.js
+++ Socket Chat Room/server/server.js
@@ -1,10 +1,15 @@
 console.log("Server is running");
 
+const {instrument} = require("@socket.io/admin-ui");
+
+
 const io = require('socket.io')(3000 ,{
     cors: {
-        origin: ['http://localhost:8080'],
+        origin: ['http://localhost:8080', 'https://admin.socket.io'],
     },
 })  // 3000 is the port number that the server will listen to for incoming connections.
+
+
 
 io.on("connection", socket=>{
     console.log("new id:",socket.id);
@@ -27,3 +32,5 @@ io.on("connection", socket=>{
 });
 
 
+instrument(io, { auth: false }) // this is for the admin ui. It is not necessary for the chat app to work.
+ 
